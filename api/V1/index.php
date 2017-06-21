8bb4bdf69c3183c6407fabdd7a21855d0278fc92
Trey Jenkins <trey@treycorp.com>
2017-06-20 23:08:59 -0400

    Developing search function

--- api/V1/index.php
+++ api/V1/index.php
@@ -9,7 +9,9 @@ function search($string) {
     $username = "USER";
     $password = "PASS";
     $hostname = "localhost";
-    
+
+    global $response;
+
     $conn = new mysqli($hostname, $username, $password, $database);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -20,12 +22,26 @@ function search($string) {
     $string = str_replace("'", "", $string);
 
     $query = explode(" ", $string);
+    $query = array_filter($query, function($value) { return $value !== ''; });
+
+    $restrk = array();
 
     foreach($query as $item) {
-        echo $item;
+        $sql = "SELECT id, timestamp, document FROM indexed WHERE tokens LIKE '%" . $item . "%'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                if (array_key_exists($row['id'], $restrk)) {
+                    $restrk[$row['id']]++;
+                } else {
+                    $restrk[$row['id']] = 1;
+                }
+            }
+        }
     }
 
-    #$sql = "SELECT id, timestamp, document FROM indexed WHERE tokens LIKE '%" . $string . "%'";
+    $response["results"] = json_encode($restrk);
 
     $conn->close();
 }
