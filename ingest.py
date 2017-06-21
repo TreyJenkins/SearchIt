cd2e19a27de8bf1169b5a7eba2feb4c8e643a8e2
Trey Jenkins <trey@treycorp.com>
2017-06-20 21:38:51 -0400

    Added ingestion script

---
+++ ingest.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import MySQLdb, sys, time
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+
+db = MySQLdb.connect(host="localhost", user=user, passwd=passwd, db="Search")
+
+def clean(data):
+    data = data.upper()
+    data = data.replace("\t", " ").replace("\n", " ").replace("\r", " ").replace("\\", " ")
+    data = data.replace(".", " ").replace(",", " ").replace("!", " ").replace("?", " ").replace(":", " ").replace(";", " ")
+    data = data.replace("'", " ").replace('"', " ")
+    data = data.split(" ")
+    data = ' '.join(data).split()
+    data = [ii for n,ii in enumerate(data) if ii not in data[:n]]
+    return ' '.join(data)
+
+infile = sys.argv[1]
+
+indata = None
+
+with open(infile, 'r') as f:
+        indata = f.read()
+
+output = {"tokens": str(clean(indata)), "timestamp": int(time.time()), "document": str(infile)}
+
+x = db.cursor()
+
+try:
+   x.execute("INSERT INTO indexed VALUES (%s, %s, %s)",(str(clean(indata)), int(time.time()), str(infile)))
+   conn.commit()
+except:
+   conn.rollback()
+
+conn.close()
