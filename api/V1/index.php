051047b11c1cab63e4e7fd80e499f6ed40791412
Trey Jenkins <trey@treycorp.com>
2017-06-20 20:09:34 -0400

    Added JSON header in API

--- api/V1/index.php
+++ api/V1/index.php
@@ -1,7 +1,8 @@
 <?php
+header('Content-Type: application/json');
 $query = htmlspecialchars($_GET["q"]);
 
-$response = array('query' => $query, 'version' => '1.0');
+$response = array("query" => $query, "version" => "1.0");
 
 echo json_encode($response);
 ?>
